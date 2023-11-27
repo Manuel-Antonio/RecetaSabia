85e9d025e443699095e219f42ae5a7e0d1a6c5e0
ManuelHuaman <manuelhuaman1998@gmail.com>
2023-11-27 03:49:29 -0500

    Proyecto Terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CarritoComponent } from './components/carrito/carrito.component';
+import { ListadoComponent } from './components/listado/listado.component';
+import { PagosComponent } from './components/pagos/pagos.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarritoComponent,
+    ListadoComponent,
+    PagosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
