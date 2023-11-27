a9a8f7ac50512fa191e978db09135bc72b886f60
ManuelHuaman <manuelhuaman1998@gmail.com>
2023-11-27 04:28:42 -0500

    sd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListadoComponent } from './components/listado/listado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: ListadoComponent    
+  },
+  {
+    path: "**",
+    redirectTo: "home"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
